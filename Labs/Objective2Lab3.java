c92e6beb816d330d7dce02fb0946ecbbefce997c
Max <maxgoduco@gmail.com>
2022-03-02 18:54:52 -0500

    Objective2 Lab 3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1 = 5, num2 = 10, num3 = 15, average = (num1 + num2 + num3)/3;
+    System.out.println("Average =" + average);
+    /*
+      Your solution goes here
+    */
+  }
+}
