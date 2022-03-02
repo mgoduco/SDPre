7092e0a00461b09498d406f054ff900c771126d2
Max <maxgoduco@gmail.com>
2022-03-02 18:50:32 -0500

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Max Goduco";
+    int age = 26;
+    String state = "Massachusetts";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
