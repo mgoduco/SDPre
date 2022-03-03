49eb705e8f3e2c7a757f8a44719ce9fc6d5d66b6
Max <maxgoduco@gmail.com>
2022-03-02 19:29:28 -0500

    Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,7 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 6, num2 = 12, num3 = 18, average = (num1 + num2 + num3)/3;
+    System.out.println("Average =" + average);
+
+  }
+}
