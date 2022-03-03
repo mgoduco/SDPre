109c515b10b4ef67443cb6545d0f9b8eef480649
Max <maxgoduco@gmail.com>
2022-03-02 23:58:43 -0500

    Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int control = 0;
+    int sum = 0;
+    for (control=1; control<=20; control++) {
+      sum += control;
+    }
+    System.out.println(sum);
+  }
+}
