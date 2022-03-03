79e4b8b4ef8d31766b51474a3a02fc040b1aed4f
Max <maxgoduco@gmail.com>
2022-03-02 23:53:00 -0500

    Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,14 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int i=1; i<=10; i++) {
+      System.out.println(i);
+    }
+    for (int a=10; a>=0; a--) {
+      System.out.println(a);
+      
+    }
+    //TODO Write another for loop that will print the numbers 10-1.
+
+
+  }
+}
